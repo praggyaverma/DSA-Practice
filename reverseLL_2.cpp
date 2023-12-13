246abf5c28c649127ffac398ed1806648d2bba03
Praggya <119279862+praggyaverma@users.noreply.github.com>
2023-12-13 14:08:16 +0530

    Create reverseLL_2.cpp

---
+++ reverseLL_2.cpp
@@ -0,0 +1,28 @@
+class Pair {
+	public :
+		
+			Node *head;
+			Node *tail;
+};
+
+Pair reverseLL_2(Node *head) {
+	if(head == NULL || head -> next == NULL) {
+		Pair ans;
+		ans.head = head;
+		ans.tail = head;
+		return ans;
+	}
+
+	Pair smallAns = reverseLL_2(head -> next);
+
+	smallAns.tail -> next = head;
+	head -> next = NULL;
+	Pair ans;
+	ans.head = smallAns.head;
+	ans.tail = head;
+	return ans;
+}
+
+Node* reverseLL_Better(Node *head) {
+	return reverseLL_2(head).head;
+}
