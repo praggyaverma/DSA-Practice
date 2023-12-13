bd30d006acd882da59be7999e9cba49fdb32b6d9
Praggya <119279862+praggyaverma@users.noreply.github.com>
2023-12-13 13:06:40 +0530

    Create detectCycleInLL.cpp

---
+++ detectCycleInLL.cpp
@@ -0,0 +1,18 @@
+bool detectCycle(Node *head)
+{
+    if (head == NULL || head -> next == NULL){
+		return false;
+	}
+	Node *slow = head;
+	Node *fast = head -> next;
+
+	while(fast != NULL && fast->next != NULL){
+        if (slow == fast){
+            return true;
+        }
+		slow = slow -> next;
+		fast = fast -> next -> next;
+	}
+
+	return false;
+}
