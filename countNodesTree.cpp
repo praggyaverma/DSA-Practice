7a86fde843f8e012ea03408a9c4d6fcb28ebc597
Praggya <119279862+praggyaverma@users.noreply.github.com>
2023-12-14 14:24:54 +0530

    Update countNodesTree.cpp

--- countNodesTree.cpp
+++ countNodesTree.cpp
@@ -1,10 +1,10 @@
 int numNodes(TreeNode<int>* root){
-  if (root == nullptr){
-    return 0;
-  }
-  int ans = 1;
-  for (int i - 0; i < root -> children.size(); i++){
-    ans += numNodes(root -> children[i])
-  }
-  return ans;
+    if (root == nullptr){
+        return 0;
+    }
+    int ans = 1;
+    for (int i - 0; i < root -> children.size(); i++){
+        ans += numNodes(root -> children[i])
+    }
+    return ans;
 }
