d8b870bb3bd0b27b9b90427a919db1b37e47d749
Praggya <119279862+praggyaverma@users.noreply.github.com>
2023-12-16 00:57:35 +0530

    Update getTreeHeight.cpp

--- getTreeHeight.cpp
+++ getTreeHeight.cpp
@@ -1,3 +1,4 @@
+// for genertic tree
 int getTreeHeight(TreeNode<int>* root) {
     if (root == nullptr){
         return 0;
@@ -10,4 +11,30 @@ int getTreeHeight(TreeNode<int>* root) {
         height = max(height, temp);
     }
     return height + 1;
+}
+
+// OR for height of Binary Tree
+
+// option 1
+int height(BinaryTreeNode<int>* root) {
+	if (root == nullptr){
+		return 0;
+	}
+
+	int h = 0;
+
+	int ans = height(root -> left);
+	h = max (ans, h);
+	ans = height(root -> right);
+	h = max(ans, h);
+
+	return 1 + h;
+}
+
+// option 2
+int height(BinaryTreeNode<int>* root) {
+	if (root == nullptr){
+		return 0;
+	}
+	return 1 + max(height(root->left), height(root->right));
 }
